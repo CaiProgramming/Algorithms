ef8042f09eb88db93169c6cdd383e1c27a76bb00
Cai Martin <cmartin.professional@yahoo.com>
2019-10-09 15:37:30 -0400

    finished stonks

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,9 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    Lowest_num_so_far = prices[0];
+    Highest_num_so_far = prices[1];
+    Max_num_loc = 1
+    max_profit_so_far = Highest_num_so_far - Lowest_num_so_far
+
+    for i in range(1,len(prices)):
+        if(Highest_num_so_far < prices[i]):
+            Highest_num_so_far = prices[i]
+            Max_num_loc = i
+    for i in range(0,Max_num_loc):
+        if(Lowest_num_so_far > prices[i]):
+            Lowest_num_so_far = prices[i]
+            max_profit_so_far = Highest_num_so_far - Lowest_num_so_far
 
 
+    return  max_profit_so_far
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
